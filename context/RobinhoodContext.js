ef5b498c045cf4ac219c762f1ca94bb1e5913133
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2022-06-22 15:01:45 +0100

    added more file to the project

---
+++ context/RobinhoodContext.js
@@ -0,0 +1,12 @@
+import { createContext, useEffect, useState } from 'react'
+
+export const RobinhoodContext = createContext()
+
+export const RobinhoodProvider = ({children}) => {
+    return (
+        <RobinhoodContext.Provider
+        value = {{}}>
+            {children}
+        </RobinhoodContext.Provider> 
+    )
+}
---
+++ pages/api/createUser.js
@@ -0,0 +1,19 @@
+//talks with sanity to create user
+
+import { client } from './../lib/sanityClient'
+
+const createUser = (req, res) => {
+    try {
+        const userDoc = {
+            _type: 'users',
+            _id: req.body.walletAddress,
+            userName: 'Unnamed',
+            address: req.body.walletAddress,
+
+        }
+    } catch (error) {
+        res.status(500).send({ message: 'error', data: error.message})
+    }
+}
+
+export default createUser
